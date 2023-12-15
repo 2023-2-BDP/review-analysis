896d00fd5e1e514d772d1a9f944b2f41e1bd00c3
박지수 <jisupark2000@gmail.com>
2023-12-15 18:56:11 +0900

    Comment: 주석 수정

--- spark-model-training.py
+++ spark-model-training.py
@@ -10,16 +10,25 @@ spark = SparkSession.builder.appName("Sentiment_Analysis").getOrCreate()
 
 
 # Train data
-train_data = spark.read.csv("hdfs:///user/maria_dev/train.csv", header="true", sep="\t", inferSchema="true")
-test_data = spark.read.csv("hdfs:///user/maria_dev/test.csv", header="true", sep="\t", inferSchema="true")
+train_data = spark.read.csv(
+    "hdfs:///user/maria_dev/train.csv", header="true", sep="\t", inferSchema="true"
+)
+test_data = spark.read.csv(
+    "hdfs:///user/maria_dev/test.csv", header="true", sep="\t", inferSchema="true"
+)
 
+# 문자열을 리스트 형식으로 변환
 list_converter_udf = udf(lambda x: eval(x) if x else [], ArrayType(StringType()))
 
+# 토큰화
 train_data = train_data.withColumn("filtered_tokens", list_converter_udf("content"))
 test_data = test_data.withColumn("filtered_tokens", list_converter_udf("content"))
 
+# CountVectorizer 모델 생성
 cv = CountVectorizer(inputCol="filtered_tokens", outputCol="raw_features")
 cv_model = cv.fit(train_data)
+
+# train data, test data 모두 vectorize
 vectorized_train_data = cv_model.transform(train_data)
 vectorized_test_data = cv_model.transform(test_data)
 
@@ -27,12 +36,16 @@ vectorized_test_data = cv_model.transform(test_data)
 lr = LogisticRegression(featuresCol="raw_features", labelCol="sentiment")
 lr_model = lr.fit(vectorized_test_data)
 
-# test data prediction
+# test data에 대한 예측 - RegressionEvaluator 사용
 predictions = lr_model.transform(vectorized_test_data)
 
-# Evaluate the model on the test set
-evaluator = RegressionEvaluator(labelCol="sentiment", predictionCol="prediction", metricName="rmse")
-accuracy = evaluator.evaluate(predictions)
-print(f"Accuracy on test data = {1 - accuracy}")
+evaluator = RegressionEvaluator(
+    labelCol="sentiment", predictionCol="prediction", metricName="rmse"
+)
 
+# Root Mean Square Error(RMSE) 계산
+accuracy = evaluator.evaluate(predictions)
+print(f"Accuracy on test data = {accuracy}")
+
+# Spark 세션 종료
 spark.stop()
--- spark_data_processing.py
+++ spark_data_processing.py
@@ -16,13 +16,23 @@ reviews = spark.read.csv("hdfs:///user/maria_dev/coupang1.csv", header="true", s
 # 필터링 및 형식 변환
 reviews = (
     reviews
+
+    # 앞의 30글자만 남기기
     .withColumn('review_content', substring(reviews['review_content'], 1, 30))
+
+    # 평점이 1~5 사이인 데이터만 남기기 (데이터 정제)
     .filter(col('rating').isin(['1', '2', '3', '4', '5']))
     .withColumn("rating", col("rating").cast(IntegerType()))
+
+    # headline, review_content가 null인 데이터 제거
     .filter((col("headline").isNull() | col("review_content").isNull()) == False)
     .withColumn("headline", when(col("headline").contains('등록된 헤드라인이'),'').otherwise(col("headline")))
     .withColumn("review_content", when(col("review_content").contains('등록된 리뷰내용이'), '').otherwise(col("review_content")))
+
+    # headline, review_content를 하나의 컬럼으로 합치기
     .withColumn("content", concat(col("headline"), lit(" "), col("review_content")))
+
+    # 중복 데이터 제거
     .dropDuplicates(['rating', 'content'])
 )
 
@@ -48,6 +58,7 @@ negative_count = reviews.filter(reviews['sentiment'] == 0).count()
 negative_count = float(negative_count)
 
 # 샘플링 후 합치기
+# 밸런스 맞추기 위해 부정적 리뷰 개수만큼 긍정적 리뷰 샘플링
 review_count = reviews.count()
 reviews_sample_positive = reviews.sampleBy("sentiment", fractions={1: negative_count/review_count}, seed=1353)
 reviews_sample_negative = reviews.sampleBy("sentiment", fractions={0: negative_count/review_count}, seed=1353)
